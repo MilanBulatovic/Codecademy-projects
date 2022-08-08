727127df619b090569c0429065519503a33965a3
Milan <milanbulatovic@outlook.com>
2022-08-09 00:59:47 +0200

    Update whaleTalk.js

--- Loops/whaleTalk.js
+++ Loops/whaleTalk.js
@@ -1,1 +1,19 @@
-test
+const input = 'This should be converted into Whale Talk'
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+const resultArray = []
+for(let i = 0; i < input.length; i++) {
+  if(input[i] === 'e' || input[i] === 'u' ) {
+      resultArray.push(input[i])
+  }
+
+  for(j = 0; j < vowels.length; j++ ) {
+    if(input[i] === vowels[j]) {
+      resultArray.push(input[i])
+    }
+  }
+}
+
+
+const resultString = resultArray.join('').toUpperCase()
+console.log(resultString)
