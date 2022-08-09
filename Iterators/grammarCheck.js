68b8b14c7bdf25037d7430f27cab1a99d7da2990
Milan <milanbulatovic@outlook.com>
2022-08-09 11:33:55 +0200

    Create grammarCheck.js

---
+++ Iterators/grammarCheck.js
@@ -0,0 +1,56 @@
+let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a stunning 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
+
+let storyWords = story.split(' ');
+let unnecessaryWord = 'literally';
+let misspelledWord = 'beautifull';
+let badWord = 'freaking';
+
+//Number of words
+let count = 0
+storyWords.forEach((word) => {
+  count++
+})
+
+console.log(count)
+
+//Filter for unnecessary word
+storyWords = storyWords.filter((word) => {
+  if(word != unnecessaryWord) {
+    return word
+  }
+});
+
+//MApping through an array and replacing misspeled word
+storyWords = storyWords.map((word) => {
+  if(word === misspelledWord){
+    return `beautiful`
+  } else {
+    return word
+  }
+})
+
+//Finding index of a certain word
+const badWordIndex = storyWords.findIndex((word) => {
+  if(word === badWord) {
+    return word
+  }
+})
+
+//Reassigning word
+storyWords[78] = 'really'
+
+//Check for words smaller than 10 characters - returning true if all words are less than 10 characters, otherwise false
+const lengthCheck = storyWords.every(word => {
+  if(word.length < 10) {
+    return word
+  }
+})
+
+//If there is a word bigger than 10 char, filter it and return it
+const check = storyWords.filter(word => {
+  if(word.length > 10) {
+    return word
+  }
+})
+ 
+console.log(check)
